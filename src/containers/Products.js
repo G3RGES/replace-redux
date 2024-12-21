660a224aca1b9e2ec1e170f8522dbd402da20e10
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-12-21 19:19:24 +0200

    Update Products.js

--- src/containers/Products.js
+++ src/containers/Products.js
@@ -1,4 +1,5 @@
-import React, { useContext } from "react";
+// import React, { useContext } from "react";
+import React from "react";
 
 import ProductItem from "../components/Products/ProductItem";
 // import { ProductsContext } from '../context/products-context';
